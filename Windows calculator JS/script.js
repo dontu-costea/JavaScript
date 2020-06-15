42bd1120eec09cc82503aecc5b37be6c7d9fa09b
dontu-costea <56258561+dontu-costea@users.noreply.github.com>
2020-06-15 14:28:40 +0300

    Add files via upload

--- Windows calculator JS/script.js
+++ Windows calculator JS/script.js
@@ -1,9 +1,15 @@
 function insert(num) {
 	document.form.textview.value = document.form.textview.value + num;
+	document.form.finput.value = document.form.finput.value + num;
+	var exp = 	document.form.textview.value;
+	if(exp) {
+		document.form.textview.value = eval(exp);
+	};
 }
 
 function clean() {
 	document.form.textview.value = "";
+	document.form.finput.value = "";
 }
 
 function back() {
@@ -12,6 +18,7 @@ function back() {
 }
 
 function equal() {
+	document.form.finput.value = "";
 	var exp = 	document.form.textview.value;
 	if(exp) {
 		document.form.textview.value = eval(exp);
